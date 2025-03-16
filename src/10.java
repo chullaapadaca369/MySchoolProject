d24be633ec0d829ebe397f605385cc8e22c87a9f
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-03-16 06:40:13 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MySchoolProject {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int number = rand.nextInt(10);
+    System.out.println("The random number is: " + number);
+  }
+}
