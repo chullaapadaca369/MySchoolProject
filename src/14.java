955da437e174a96a4dbb6b2f3351d6a072b76cd6
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-03-20 12:22:27 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,21 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example of how to initialize variables in Java
+        int x = 5;
+        String name = "Alice";
+        
+        // Example of using if-else statements
+        if (x < 10) {
+            System.out.println("Value of x is less than 10");
+        } else {
+            System.out.println("Value of x is not less than 10");
+        }
+        
+        // Example of using loops
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
