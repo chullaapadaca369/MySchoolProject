fe70956f39dc639dc055becadb49fb595d052409
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-03-31 09:06:40 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,10 @@
+public class SimpleSchoolProject {
+    public static void main(String[] args) {
+        // Example: Input and output operations in Java
+        String input = "Hello, World!";
+        String output = input + " Programming is fun.";
+        
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + output);
+    }
+}
