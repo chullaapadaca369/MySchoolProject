fd0ca105b9b841e4a49f211edbe434d734e65bee
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-04-01 17:34:18 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,17 @@
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Sample code for a student to start their first day of school
+
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to our school!");
+
+        String name = "John Doe";
+        System.out.println(name + " is the student's name.");
+
+        int age = 12;
+        System.out.println(age + " is John's age.");
+
+        boolean isFreshman = true;
+        System.out.println(isFreshman + " is a freshman.");
+    }
+}
