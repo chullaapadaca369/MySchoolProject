0d211741fe4d58454f30a471563d32a4a11beaaf
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-05-20 08:10:30 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,6 @@
+public class MySchoolProject {
+    public static void main(String[] args) {
+        // Code for a simple school project goes here
+        System.out.println("Hello, this is a simple school project!");
+    }
+}
