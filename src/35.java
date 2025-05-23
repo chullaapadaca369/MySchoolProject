ce1cb182e14d4fedc78a50f3d63a625191bf7f51
chullaapadaca369 <chullaapadaca369@gmail.com>
2025-05-23 05:31:49 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,9 @@
+public class SimpleJavaProject {
+
+    // Example method to print "Hello, World!" repeatedly.
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
